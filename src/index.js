75712da2fabb4144e099de190755985ede3a3ff5
Dmytro <solonar96@gmail.com>
2024-05-21 14:04:28 +0300

    Added manifest.json (toprevent errors in console), added RandomChar functionality

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import App from './components/app/App';
+
 import './style/style.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
