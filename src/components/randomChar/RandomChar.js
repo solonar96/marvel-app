1c19f361d4c16a7ae2b4b99ebe3ed2d293a98b47
Dmytro <solonar96@gmail.com>
2024-05-31 18:53:33 +0300

    Solved some bugs, added charInfo functionality, added ErrorBoundary functionality

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -24,7 +24,7 @@ class RandomChar extends Component {
 		this.setState({char, loading: false});
 	}
 
-	inCharLoading = () => {
+	onCharLoading = () => {
 		this.setState({loading: true})
 	}
 
@@ -34,7 +34,7 @@ class RandomChar extends Component {
 
 	updateChar = () => {
 		const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
-		this.inCharLoading();
+		this.onCharLoading();
 		this.marvelService
 			.getCharacter(id)
 			.then(this.onCharLoaded)
