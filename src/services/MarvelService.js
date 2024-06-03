9537b6774ed6ed2cad6033ebb90cc6b658269606
Dmytro <solonar96@gmail.com>
2024-06-03 20:03:37 +0300

    Added CharList pagination functionality, MarvelService fixed some buggs, added prop-types dependencies

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -3,6 +3,7 @@
 class MarvelService {
 	_apiBase = 'https://gateway.marvel.com:443/v1/public/';
 	_apiKey = 'apikey=6a4cef57bd1deb3a6136d07531ed4817';
+	_baseOffset = 215;
 
 	getResource = async (url) => {
 		let res = await fetch(url);
@@ -14,8 +15,8 @@ class MarvelService {
 		return await res.json();
 	}
 
-	getAllCharacters = async () => {
-		const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=215&${this._apiKey}`);
+	getAllCharacters = async (offset = this._baseOffset) => {
+		const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
 		return res.data.results.map(this._transformCharacter);
 	}
 
